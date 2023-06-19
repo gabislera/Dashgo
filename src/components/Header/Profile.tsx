9e382f08a9b9650eefc3fcd3a454f01b13d058dc
Gabriela Carniel <gabiicarniel@gmail.com>
2023-06-19 18:55:34 -0300

    responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align='center'>
-      <Box mr='4' textAlign='right'>
-        <Text>Gabriela Carniel</Text>
-        <Text color='gray.300' fontSize='sm'>gabiicarniel@gmail.com</Text>
-      </Box>
+      {showProfileData && (
+        <Box mr='4' textAlign='right'>
+          <Text>Gabriela Carniel</Text>
+          <Text color='gray.300' fontSize='sm'>gabiicarniel@gmail.com</Text>
+        </Box>
+      )}
 
     <Avatar size='md' name='Gabriela Carniel' src='https://github.com/gabislera.png'/>
   </Flex>
