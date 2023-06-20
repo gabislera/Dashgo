c8ac2d32c4d9c49d9ac78b9b7238d7df68f2b501
Gabriela Carniel <gabiicarniel@gmail.com>
2023-06-20 09:58:54 -0300

    context template

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,22 +1,31 @@
-import { Box, Stack } from '@chakra-ui/react'
-import { RiDashboardLine, RiContactsLine, RiInputMethodLine, RiGitMergeLine } from 'react-icons/ri'
-import { NavLink } from './NavLink'
-import { NavSection } from './NavSection'
+import { Box, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, useBreakpointValue } from '@chakra-ui/react'
+import { SidebarNav } from './SidebarNav'
 
 export function Sidebar() {
+  const isDrawerSidebar = useBreakpointValue({
+    base: true,
+    lg: false,
+  })
+
+  if (isDrawerSidebar) {
+    return (
+    <Drawer isOpen={true} placement='left' onClose={() => {}}>
+      <DrawerOverlay>
+        <DrawerContent bg='gray.800' p='4'>
+          <DrawerCloseButton mt='6' />
+          <DrawerHeader>Navegação</DrawerHeader>
+
+          <DrawerBody>
+            <SidebarNav />
+          </DrawerBody>
+        </DrawerContent>
+      </DrawerOverlay>
+    </Drawer>
+  )}
+
   return (
     <Box as='aside' w='64' mr='8'>
-      <Stack spacing='12' align='flex-start'>
-        <NavSection title='GERAL'>
-          <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-          <NavLink icon={RiContactsLine}>Usuários</NavLink>
-        </NavSection>
-
-        <NavSection title='AUTOMAÇÃO'>
-          <NavLink icon={RiInputMethodLine}>Formulários</NavLink>
-          <NavLink icon={RiGitMergeLine}>Automação</NavLink>
-        </NavSection>
-      </Stack>
+      <SidebarNav />
     </Box>
   )
 }
