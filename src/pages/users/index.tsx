4fbabbe1059a01fa40b6d0b31f4ec714dc465d66
Gabriela Carniel <gabiicarniel@gmail.com>
2023-06-22 10:07:20 -0300

    axios config

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,11 +5,11 @@ import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import { Pagination } from '../../components/Pagination'
 import Link from 'next/link'
 import { useQuery } from 'react-query'
+import { api } from '../../services/api'
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
+  const { data, isLoading, error, isFetching } = useQuery('users', async () => {
+    const { data } = await api.get('users')
 
     const users = data.users.map(user => {
       return {
@@ -44,7 +44,11 @@ export default function UserList() {
 
         <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
           <Flex mb='8' justify='space-between' align='center'>
-            <Heading size='lg' fontWeight='normal'>Usuários</Heading>
+            <Heading size='lg' fontWeight='normal'>
+              Usuários
+
+              { !isLoading && isFetching && <Spinner size='sm' color='gray.500' ml='4'/>}
+            </Heading>
 
               <Button href='/users/create' as={Link} size='sm' fontSize='sm' colorScheme='pink' leftIcon={<Icon as={RiAddLine} fontSize='20'/>}>
                 Criar novo
